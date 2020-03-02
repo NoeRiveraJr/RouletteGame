975ad5312590ba021b0b0913087cb82631079e9e
NoeRiveraJr <noe.rivera.jr@outlook.com>
2020-03-02 15:40:43 -0500

    Adding the first 5 methods to the bets class representing 5 different bets that are possible at a roulette table.

---
+++ RouletteGame/Roulette.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouletteGame
+{
+    class Roulette
+    {
+        static void Main(string[] args)
+        {
+            string[] rouletteNumbers = new string[38];
+            string[] rouletteColors = new string[38] {"green", "red", "black", "red", "black", "red", "black", "red", "black", "red", "black", "black", "red", "black",
+            "red", "black", "red", "black", "red", "red", "black", "red", "black", "red", "black", "red", "black","red", "black", "black", "red", "black", "red","black",
+            "red", "black", "red", "green"};
+            int gameFinish = 0;
+            Random r = new Random();
+            RouletteBets rb = new RouletteBets();
+
+            for (int i = 0; i < rouletteNumbers.Length; i++)
+            {
+                if (i == 37)
+                {
+                    rouletteNumbers[i] = "00";
+                }
+                else
+                {
+                    rouletteNumbers[i] = i.ToString();
+                }
+            }
+
+            do
+            {
+                Console.WriteLine("Welcome to Roulette table at my casino! Please enter a follwing menu option to play!\n1.Spin the wheel and see all possible winning bets!\n2.Exit the Program");
+                string check = Console.ReadLine();
+                int x;
+                int spotIndex = r.Next(0, 38);
+                string landingSpot = rouletteNumbers[spotIndex];
+                while (Int32.TryParse(check, out x) == false || x != 1 && x != 2)
+                {
+                    Console.WriteLine("Please enter a valid menu option!");
+                    check = Console.ReadLine();
+                }
+                switch (x)
+                {
+                    case 1:
+                        Console.WriteLine($"The ball has landed on number {landingSpot}.");
+                        Console.WriteLine($"1. The winning number bet was {landingSpot}.");
+                        Console.WriteLine($"2. {rb.evenOddBet(rouletteNumbers,spotIndex)}");
+                        Console.WriteLine($"3. The winning color bet is {rb.colorBet(rouletteColors, spotIndex)}.");
+                        Console.WriteLine($"4. {rb.lowHighBet(rouletteNumbers, spotIndex)}");
+                        Console.WriteLine($"5. {rb.dozenBet(rouletteNumbers, spotIndex)}");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 2:
+                        gameFinish++;
+                        break;
+                }
+            } while (gameFinish != 1);
+
+            Console.WriteLine("Thanks for playing roulette at my casino!");
+
+
+
+
+
+        }
+    }
+}
+
+
+    
---
+++ RouletteGame/RouletteBets.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouletteGame
+{
+    class RouletteBets
+    {
+        public string evenOddBet(string[] rouletteNumber, int spotIndex)
+        {
+            string oddOrEven;
+            if (spotIndex == 0)
+            {
+                oddOrEven = "0 is not odd or even, neither is a winning bet.";
+            }
+            else if (spotIndex==37)
+            {
+                oddOrEven = "00 is not odd or even, neither is a winning bet.";
+            }
+            else
+            {
+                if(Int32.Parse(rouletteNumber[spotIndex]) % 2 == 0)
+                {
+                    oddOrEven = ($"The winning bet is even because the number {rouletteNumber[spotIndex]} is even.");
+                }
+                else
+                {
+                    oddOrEven = ($"The winning bet is odd because the number {rouletteNumber[spotIndex]} is odd.");
+                }
+            }
+            return oddOrEven;
+        }
+
+        public string colorBet(string[] rouletteColors, int spotIndex)
+        {
+            string color = rouletteColors[spotIndex];
+            return color;
+        }
+
+        public string lowHighBet(string[] rouletteNumbers, int spotIndex)
+        {
+            string lowOrHigh;
+            if (spotIndex == 0)
+            {
+                lowOrHigh = "0 is not a low or high number, neither is a winning bet.";
+            }
+            else if (spotIndex == 37)
+            {
+                lowOrHigh = "00 is not a low or high number, niether is a winning bet.";
+            }
+            else
+            {
+                if(Int32.Parse(rouletteNumbers[spotIndex]) > 0 && Int32.Parse(rouletteNumbers[spotIndex]) <= 18)
+                {
+                   lowOrHigh = ($"The winning bet is a low number because {rouletteNumbers[spotIndex]} is between 1 - 18.");
+                }
+                else
+                {
+                    lowOrHigh = ($"The winning bet is a high number because {rouletteNumbers[spotIndex]} is between 19 - 36.");
+                }
+            }
+            return lowOrHigh;
+        }
+
+        public string dozenBet(string[] rouletteNumbers, int spotIndex)
+        {
+            string dozenPlace;
+            if (spotIndex == 0)
+            {
+               dozenPlace = "0 does not fall in a dozen, neither would be a winning bet.";
+            }
+            else if (spotIndex == 37)
+            {
+                dozenPlace = "00 does not fall in a dozen, neither would be a winning bet.";
+            }
+            else
+            {
+                if (Int32.Parse(rouletteNumbers[spotIndex]) > 0 && Int32.Parse(rouletteNumbers[spotIndex]) <= 12)
+                {
+                    dozenPlace = ($"The winning bet is the first dozen because {rouletteNumbers[spotIndex]} is between 1 - 12.");
+                }
+                else if (Int32.Parse(rouletteNumbers[spotIndex]) > 12 && Int32.Parse(rouletteNumbers[spotIndex]) <= 24)
+                {
+                    dozenPlace = ($"The winning bet is the second dozen because {rouletteNumbers[spotIndex]} is between 13 - 24.");
+                }
+                else
+                {
+                    dozenPlace = ($"The winning bet is the third dozen because {rouletteNumbers[spotIndex]} is between 25 - 36.");
+                }
+            }
+            return dozenPlace;
+        }
+    }
+
+    
+}
